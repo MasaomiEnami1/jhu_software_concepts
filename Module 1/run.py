91e66e13ccc1b3a21b3e3ba608fefd60c40ba063
MasaomiEnami1 <masaoenami@gmail.com>
2026-02-01 20:08:40 -0800

    push testing

--- Module 1/run.py
+++ Module 1/run.py
@@ -3,3 +3,4 @@ from app import app # This refers to the 'app' variable inside the 'app' folder
 if __name__ == "__main__":
     # Using Port 8080
     app.run(host='0.0.0.0', port=8080, debug=True)
+    
