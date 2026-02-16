65ab907d810dba7bd01a2ca32c9727e6d05f3604
MasaomiEnami1 <masaoenami@gmail.com>
2026-02-15 16:33:11 -0800

    7th push module 4

---
+++ Module 4/source/conf.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+# We go up two levels from docs/source to find the project root, then into /src
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# -- Project information -----------------------------------------------------
+project = 'Grad Cafe Analytics'
+copyright = '2026, Masaomi Enami'
+author = 'Masaomi Enami'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# You MUST add these extensions to fulfill the 'autodoc' requirement
+extensions = [
+    'sphinx.ext.autodoc',    # Pulls docstrings from your code
+    'sphinx.ext.napoleon',   # Makes docstrings look professional
+    'sphinx.ext.viewcode',   # Adds 'source' links in the docs
+    'sphinx_rtd_theme',  
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ Module 4/source/index.rst
@@ -0,0 +1,10 @@
+Grad Cafe Analytics Documentation
+=================================
+
+.. automodule::add
+   :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
