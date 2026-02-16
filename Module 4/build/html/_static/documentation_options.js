548d6e93cfa47a75d7f2ca30052d6f9d5be36035
MasaomiEnami1 <masaoenami@gmail.com>
2026-02-15 16:57:26 -0800

    8th push module 4

--- Module 4/build/html/_static/documentation_options.js
+++ Module 4/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1',
-    LANGUAGE: 'En',
+    VERSION: '1.0',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
