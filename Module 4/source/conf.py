b65f591de9beec59ee906ae787e30c6ab431cc0e
MasaomiEnami1 <masaoenami@gmail.com>
2026-02-15 17:17:12 -0800

    10th push

--- Module 4/source/conf.py
+++ Module 4/source/conf.py
@@ -1,8 +1,6 @@
 import os
 import sys
-
-# -- Path setup --------------------------------------------------------------
-# We go up two levels from docs/source to find the project root, then into /src
+# This tells Sphinx to look in the src folder relative to this conf.py file
 sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information -----------------------------------------------------
